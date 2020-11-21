44ca8384b8a21915369feeef80bb082fbbcd6f2c
arq-gabo <jgabrielguerral@gmail.com>
2020-11-21 18:14:49 -0400

    [FEATURE]Google Analytics Offline

--- src/service-worker.js
+++ src/service-worker.js
@@ -12,6 +12,9 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {})
 // App Shell
 workbox.routing.registerNavigationRoute('/index.html')
 
+//Google Analitics
+workbox.googleAnalytics.initialize()
+
 workbox.routing.registerRoute(/^https?:\/\/www.themealdb.com\/api\/.*/,
     workbox.strategies.staleWhileRevalidate(),
     'GET')
